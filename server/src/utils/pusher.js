22b76f236e5dfde91f0c5f1f2fbb4b1cc060c743
linoy <linoy800081@gmail.com>
2023-07-12 15:30:15 +0300

    add comments

--- server/src/utils/pusher.js
+++ server/src/utils/pusher.js
@@ -1,6 +1,10 @@
 const Pusher = require('pusher');
 const config = require('../config');
 
+/**
+ * Initializes and returns a new Pusher instance.
+ * @returns { Pusher } 
+ */
 const initPusher = () => {
     return new Pusher({
         appId: config('pusher:id'),
